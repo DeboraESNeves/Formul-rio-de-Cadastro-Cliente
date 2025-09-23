d5a8f70d9ce94eab7084ba4e2a714d6e01079d73
Debora Neves <deboraesneves.dn@gmail.com>
2025-09-23 14:22:43 -0300

    Projeto com .gitignore corrigido e arquivos limpos

---
+++ Formulario Cadastro Cliente/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Formulario_Cadastro_Cliente.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Formulario_Cadastro_Cliente.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Formulario Cadastro Cliente/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Formulario_Cadastro_Cliente.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
